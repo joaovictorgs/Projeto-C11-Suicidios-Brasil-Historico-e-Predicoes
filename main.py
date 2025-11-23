8ed34c84d05dd46998f51f6a7ee0d18f040a0697
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-11-23 02:29:11 -0300

    chore: update main to use Brazil and Japan data processing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from src.data_processing import process_brazil_data
+from src.data_processing import process_brazil_data, process_japan_data
 
 
 def main():
@@ -9,9 +9,12 @@ def main():
         input_filepath='data/combined_processed_data.csv',
         output_filepath='data/brazil_aggregated.csv'
     )
-    
+    df_japan = process_japan_data(
+        input_filepath='data/combined_processed_data.csv',
+        output_filepath='data/japan_aggregated.csv'
+    )
     print(df_brazil)
-
+    print(df_japan)
 
 if __name__ == "__main__":
     main()
